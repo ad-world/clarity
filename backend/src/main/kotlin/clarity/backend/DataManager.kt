3ad46f078f1c229123933878c471925f4951dc8b
ad-world <67097683+ad-world@users.noreply.github.com>
2023-06-19 22:37:13 -0400

    Create User Table + add way to execute SQL files with ease

--- backend/src/main/kotlin/clarity/backend/DataManager.kt
+++ backend/src/main/kotlin/clarity/backend/DataManager.kt
@@ -1,9 +1,9 @@
 package clarity.backend
 
 import java.io.File
+import java.nio.charset.Charset
 import java.sql.Connection
 import java.sql.DriverManager
-
 class DataManager {
     private val db = DriverManager.getConnection("jdbc:sqlite:data.sqlite")
 
@@ -13,13 +13,7 @@ class DataManager {
     }
 
     private fun createSampleTable() {
-        try {
-            val statement = db.createStatement()
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Data (id integer, name varchar(255))")
-            statement.executeUpdate("INSERT INTO Data VALUES(1, 'Aryaman')")
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        executeSqlFile("ddl/CREATE_USER.sql");
     }
 
     private fun createDbFile() {
@@ -27,6 +21,26 @@ class DataManager {
         if(!file.exists()) {
             val success = file.createNewFile()
             println(success)
+        }
+    }
+
+    /*
+    Use this function if you want to run one sql statement from a file (such as a CREATE TABLE statement)
+     */
+    fun executeSqlFile(pathInsideSQLFolder: String) {
+        val sqlFile = File("src/main/sql/$pathInsideSQLFolder")
+        if(sqlFile.exists()) {
+            try {
+                val contents = sqlFile.readText(Charset.defaultCharset());
+                val statement = db.createStatement()
+                statement.executeUpdate(contents)
+                statement.close()
+            } catch (e: Exception) {
+                e.printStackTrace();
+            }
+
+        } else {
+            println("${sqlFile.absolutePath} does not exist. Please fix your file path");
         }
     }
 
