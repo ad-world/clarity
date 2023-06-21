1053cd269ea0a9b7fb8d4ffde55cd0d3f38961e8
ad-world <67097683+ad-world@users.noreply.github.com>
2023-06-21 19:06:24 -0400

    Create user endpoint

--- backend/src/main/kotlin/clarity/backend/controllers/LoginController.kt
+++ backend/src/main/kotlin/clarity/backend/controllers/LoginController.kt
@@ -1,8 +1,7 @@
 package clarity.backend.controllers
 
 import clarity.backend.DataManager
-import clarity.backend.entity.UserEntity
-import clarity.backend.entity.UserLoginEntity
+import clarity.backend.entity.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -22,6 +21,20 @@ class LoginController {
             return ResponseEntity.ok("Login successful")
         }
         return ResponseEntity.badRequest().body("Invalid credentials")
+    }
+
+    @PostMapping("/createUser")
+    fun createUser(@RequestBody user: CreateUserEntity): ResponseEntity<String> {
+        val userEntity = UserEntity(db);
+
+        val newUserResponse = userEntity.createUser(user)
+
+        return if(newUserResponse.response == StatusResponse.Success) {
+            ResponseEntity.ok(newUserResponse.message)
+        } else {
+            ResponseEntity.badRequest().body(newUserResponse.message)
+        }
+
     }
 
 
--- backend/src/main/kotlin/clarity/backend/entity/UserEntity.kt
+++ backend/src/main/kotlin/clarity/backend/entity/UserEntity.kt
@@ -2,7 +2,14 @@ package clarity.backend.entity
 
 import clarity.backend.DataManager
 
+enum class StatusResponse {
+    Success,
+    Failure
+}
+
 data class UserLoginEntity(val username: String, val password: String)
+data class CreateUserEntity(val username: String, val email: String, val password: String, val firstname: String, val lastname: String, val phone_number: String)
+data class CreateUserResponse(val response: StatusResponse, val message: String)
 
 class UserEntity(dataManager: DataManager) {
     private val db = dataManager.db
@@ -20,6 +27,25 @@ class UserEntity(dataManager: DataManager) {
         } catch (e: Exception) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    fun createUser(user: CreateUserEntity): CreateUserResponse {
+        try {
+            val statement = db.createStatement()
+            val insertStatement = """
+                INSERT INTO User (username, first_name, last_name, email, password, phone_number)
+                VALUES(
+                '${user.username}', '${user.firstname}', '${user.lastname}', 
+                '${user.email}', '${user.password}', '${user.phone_number}'
+                )
+            """.trimIndent()
+            val result = statement.executeUpdate(insertStatement)
+
+            return CreateUserResponse(StatusResponse.Success, "User created successfully")
+        } catch(e: Exception) {
+            e.printStackTrace();
+            return CreateUserResponse(StatusResponse.Failure, e.message ?: "Unknown error")
         }
     }
 }
