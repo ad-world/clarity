ade6b038a33ded2c2fde82f4d6132ca97ab4fbaf
Arshpreet Chabbewal <aschabbe@uwaterloo.ca>
2023-06-24 19:29:10 -0400

    Sets UI: Added basic logic for creating sets, and displaying sets

--- android/app/src/main/java/com/example/clarity/setspage/Set.kt
+++
@@ -1,8 +0,0 @@
-package com.example.clarity.setspage
-
-data class Set(
-    val title: String,
-    val cardCount: Int,
-    val cards: MutableList<Card>,
-    val progress: Float,
-)
--- android/app/src/main/java/com/example/clarity/setspage/SetsController.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clarity.setspage
-
-class SetsController {
-    
-
-}
