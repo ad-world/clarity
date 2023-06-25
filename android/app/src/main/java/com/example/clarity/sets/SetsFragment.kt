9659bd95126e7ddaf73aeefb0eee959ca49e0bce
Arshpreet Chabbewal <aschabbe@uwaterloo.ca>
2023-06-25 17:16:50 -0400

    sets ui: Updated Sets list ui, and added onclick

--- android/app/src/main/java/com/example/clarity/sets/SetsFragment.kt
+++ android/app/src/main/java/com/example/clarity/sets/SetsFragment.kt
@@ -1,65 +1,107 @@
 package com.example.clarity.sets
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.clarity.R
+import com.example.clarity.databinding.FragmentSetsBinding
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val USER_ID = "userId"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SetsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SetsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SetsFragment() : Fragment() {
+
+    private var userId: Int? = 0
+    private var _binding: FragmentSetsBinding? = null
+    private lateinit var setAdapter: SetAdapter
+    private lateinit var sets: MutableList<Set>
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private fun onSetClick(position: Int) {
+        val intent = Intent(activity, TestSetActivity::class.java)
+        intent.putExtra("setId", sets[position].id);
+        intent.putExtra("userId", userId);
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            userId = it.getInt(USER_ID)
         }
+
+        sets = mutableListOf()
+        // TODO: Replace following lines with a query for all sets with our userId, and then parse
+        //  through the object returned, creating a set data class for each set, and appending it
+        //  to the sets array
+
+        // TEST DATA, will be removed when database is connected
+        sets.add(Set(0, "Animals", 4,
+            mutableListOf(Card("Dog", false),
+                Card("Cat", false),
+                Card("Zebra", false),
+                Card("Kangaroo", false)),
+            0, SetCategory.DEFAULT_SET))
+
+        sets.add(Set(1, "Countries", 3,
+            mutableListOf(Card("Canada", false),
+                Card("Russia", false),
+                Card("Japan", false)),
+            0, SetCategory.DOWNLOADED_SET))
+
+        sets.add(Set(2, "Devices", 5,
+            mutableListOf(Card("Phone", false),
+                Card("Laptop", false),
+                Card("Computer", false),
+                Card("Television", false),
+                Card("Tablet", false)),
+            0, SetCategory.COMMUNITY_SET))
+
+        setAdapter = SetAdapter(sets) { position -> onSetClick(position) }
+        binding.rvSets.adapter = setAdapter
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sets, container, false)
+        _binding = FragmentSetsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        R.id.rvSets
+        binding.btnCreateSet.setOnClickListener {
+            val intent = Intent(activity, CreateSetActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
+        // This uses a design pattern lol are we allowed to say that this is one of the ones we implemented
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param userId User ID of user that logged in
          * @return A new instance of fragment SetsFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(userId: Int) =
             SetsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(USER_ID, userId)
                 }
             }
     }
