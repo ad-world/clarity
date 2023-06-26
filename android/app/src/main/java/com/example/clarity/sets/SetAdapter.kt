f5d037d96c6a05591ca1bb4d2c268d7940f67726
Arshpreet Chabbewal <arsh.c321@gmail.com>
2023-06-26 11:50:06 -0400

    sets ui: Updated set list

--- android/app/src/main/java/com/example/clarity/sets/SetAdapter.kt
+++ android/app/src/main/java/com/example/clarity/sets/SetAdapter.kt
@@ -4,12 +4,15 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.clarity.R
 
-class SetAdapter (
+class SetAdapter(
     private val sets: MutableList<Set>,
+    private val userId: Int?,
     private val onItemClicked: (position: Int) -> Unit
 ) : RecyclerView.Adapter<SetAdapter.SetViewHolder>() {
 
@@ -39,23 +42,22 @@ class SetAdapter (
         return sets.size
     }
 
-    fun addSet(set: Set) {
-        sets.add(set)
-        notifyItemInserted(sets.size - 1)
-    }
-
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: SetViewHolder, position: Int) {
         val set = sets[position]
+        println("Position: $position")
         holder.itemView.apply {
-            val setTitle = findViewById<TextView>(R.id.tvSetTitle)
+            val tvSetTitle = findViewById<TextView>(R.id.tvSetTitle)
+            val tvCardCount = findViewById<TextView>(R.id.tvCardCount)
+            val tvCompletedPhrases = findViewById<TextView>(R.id.tvCompletedPhrases)
+            val tvCompletedPercent = findViewById<TextView>(R.id.tvCompletedPercent)
+            val progressBarSet = findViewById<ProgressBar>(R.id.progressBarSet)
 
-            setTitle.text = "Title: ${set.title}\n" +
-                    "Count: ${set.cards.size}\n" +
-                    "Category: ${set.category}\n" +
-                    "Progress: ${set.progress}" +
-                    "Card Phrase 1: "
-
+            tvSetTitle.text = set.title
+            tvCardCount.text = set.cards.size.toString()
+            tvCompletedPhrases.text = "${set.progress} Completed"
+            tvCompletedPercent.text = "${set.progress / set.cards.size}%"
+            progressBarSet.progress = set.progress / set.cards.size
         }
     }
 }
