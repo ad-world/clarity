299fdd926a3b97d35b7491ec68515fa36a96fd47
Guneet Bola <guneetbola@gmail.com>
2023-06-27 15:14:15 -0400

    finished ui for login signup and profile

--- android/app/src/main/java/com/example/clarity/SecondFragment.kt
+++ android/app/src/main/java/com/example/clarity/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.clarity
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -41,7 +42,18 @@ class SecondFragment : Fragment() {
 
             //call api
 
-            findNavController().navigate(R.id.FirstFragment) //will go to login page once they signup for now
+            val successfulSignup = true
+
+            if(successfulSignup) {
+                val intent = Intent(activity, IndexActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.editTextUsername.setText("")
+                binding.editTextPassword.setText("")
+                binding.editTextName.setText("")
+                binding.editTextEmail.setText("")
+                unsuccessfulSignUp()
+            }
         }
 
         binding.loginLink.setOnClickListener {
@@ -51,6 +63,15 @@ class SecondFragment : Fragment() {
 
     }
 
+    private fun unsuccessfulSignUp() {
+        val alertDialog = AlertDialog.Builder(requireContext()).create() //the method to get context might be incorrect
+        alertDialog.setMessage("Unsuccessful Sign-Up. Please try again.")
+        alertDialog.setTitle("Incorrect Credentials")
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        alertDialog.show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
