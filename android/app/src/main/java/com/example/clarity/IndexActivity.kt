7e747d391cc9f5b5eb352da4a72471b487f6c195
Guneet Bola <guneetbola@gmail.com>
2023-06-27 15:14:15 -0400

    finished ui for login signup and profile

--- android/app/src/main/java/com/example/clarity/IndexActivity.kt
+++ android/app/src/main/java/com/example/clarity/IndexActivity.kt
@@ -14,6 +14,7 @@ import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.example.clarity.databinding.ActivityMainBinding
 import com.example.clarity.databinding.IndexActivityBinding
+import com.example.clarity.setspage.SetsFragment
 
 // Just created a blank file for the main content
 
--- android/app/src/main/java/com/example/clarity/ProfileFragment.kt
+++ android/app/src/main/java/com/example/clarity/ProfileFragment.kt
@@ -1,59 +1,120 @@
 package com.example.clarity
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.findNavController
+import com.example.clarity.databinding.FragmentProfileBinding
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.google.android.material.navigation.NavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileBinding? = null
+
+    private val binding get() = _binding!!
+    //private lateinit var lineChart: LineChart
+    private lateinit var pieChart: PieChart
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        //navigation
+
+        val dropdownView: ImageView = binding.dropdown
+
+
+        dropdownView.setOnClickListener {
+            val menu = PopupMenu(view.context, dropdownView)
+            menu.inflate(R.menu.profile_menu) // Replace with your menu resource file
+            menu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_settings -> {
+                        true
+                    }
+                    R.id.logout -> {
+                        logout()
+                        true
+                    }
+                    // Add more menu item cases as needed
+                    else -> false
                 }
             }
+            menu.show()
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+//        lineChart = binding.lineChart
+//
+//
+//        //line chart:
+//        //lineChart.description.isEnabled = false
+//        lineChart.setTouchEnabled(true)
+//        lineChart.setPinchZoom(true)
+
+
+
+        val totalSavedSets = 100
+        var completedSets = 40
+        var incompleteSets = 20
+        var notStartedSets = 60
+
+        //pie chart:
+        pieChart = binding.pieChart
+        val entries = mutableListOf<PieEntry>()
+        entries.add(PieEntry(completedSets.toFloat() / totalSavedSets, "Completed"))
+        entries.add(PieEntry((incompleteSets.toFloat() / totalSavedSets), "Incomplete"))
+        entries.add(PieEntry((notStartedSets.toFloat() / totalSavedSets), "Not Started"))
+        val dataSet = PieDataSet(entries, "")
+        val colourList = listOf(
+            Color.parseColor("#C6C6C6"), //light grey
+            Color.parseColor("#74ABFF"), //light blue
+            Color.parseColor("#3546D9") //blue
+        )
+        dataSet.colors = colourList
+        val data = PieData(dataSet)
+        pieChart.setUsePercentValues(true)
+        pieChart.setDrawEntryLabels(false)
+        pieChart.description.isEnabled = false
+        pieChart.data = data
+        pieChart.invalidate()
+
+
+        //number of sets completed
+        binding.completedSetsNum.text = completedSets.toString()
+    }
+
+    private fun logout() {
+        val intent = Intent(activity, MainActivity::class.java)
+        startActivity(intent)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
