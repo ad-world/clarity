ff6f64dbc87d5850ef0fe141d130347e847c2b34
Arshpreet Chabbewal <arsh.c321@gmail.com>
2023-06-27 22:38:26 -0400

    sets ui: fixed logic errors in create set activity

--- android/app/src/main/java/com/example/clarity/sets/CreateSetActivity.kt
+++ android/app/src/main/java/com/example/clarity/sets/CreateSetActivity.kt
@@ -1,9 +1,11 @@
 package com.example.clarity.sets
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.clarity.R
@@ -12,10 +14,15 @@ import com.google.android.material.textfield.TextInputEditText
 class CreateSetActivity : AppCompatActivity() {
     private lateinit var cardAdapter: CardAdapter
 
+    fun HideKeyboard() {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(View(this).windowToken, 0)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_set)
-        cardAdapter = CardAdapter(mutableListOf())
+        cardAdapter = CardAdapter(mutableListOf()){ HideKeyboard() }
         val rvCards = findViewById<RecyclerView>(R.id.rvCards)
         rvCards.adapter = cardAdapter
         rvCards.layoutManager = LinearLayoutManager(this)
