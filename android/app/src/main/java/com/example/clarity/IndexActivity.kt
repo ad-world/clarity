195f3325e69f524cb9b74f44dbc99e43f0f17570
Arshpreet Chabbewal <arsh.c321@gmail.com>
2023-07-12 16:02:58 -0400

    removed comment

--- android/app/src/main/java/com/example/clarity/IndexActivity.kt
+++ android/app/src/main/java/com/example/clarity/IndexActivity.kt
@@ -1,7 +1,6 @@
 package com.example.clarity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.clarity.classroompage.ClassroomFragment
