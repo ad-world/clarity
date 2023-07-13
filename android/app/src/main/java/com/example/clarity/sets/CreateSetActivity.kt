7e8bf0dba6b25db1c8c196341a75407062117eaa
Arshpreet Chabbewal <arsh.c321@gmail.com>
2023-07-13 19:32:06 -0400

    connect create set to backend endpoint

--- android/app/src/main/java/com/example/clarity/sets/CreateSetActivity.kt
+++ android/app/src/main/java/com/example/clarity/sets/CreateSetActivity.kt
@@ -7,19 +7,31 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.clarity.sdk.ClaritySDK
 import com.example.clarity.sdk.CreateCardSetEntity
 import com.example.clarity.sdk.CreateCardSetResponse
 import com.example.clarity.R
+import com.example.clarity.SessionManager
+import com.example.clarity.sdk.AddCardToSetRequest
+import com.example.clarity.sdk.AddCardToSetResponse
+import com.example.clarity.sdk.CreateCardEntity
+import com.example.clarity.sdk.CreateCardResponse
+import com.example.clarity.sdk.CreateUserEntity
+import com.example.clarity.sdk.CreateUserResponse
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import retrofit2.Response
 
 class CreateSetActivity : AppCompatActivity() {
     private lateinit var cardAdapter: CardAdapter
     private val api = ClaritySDK().apiService
+    private val sessionManager: SessionManager by lazy { SessionManager(this) }
+
+    private var userid = 0
 
     fun HideKeyboard() {
         val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -33,6 +45,10 @@ class CreateSetActivity : AppCompatActivity() {
         val rvCards = findViewById<RecyclerView>(R.id.rvCards)
         rvCards.adapter = cardAdapter
         rvCards.layoutManager = LinearLayoutManager(this)
+
+        lifecycleScope.launch {
+            userid = sessionManager.getUserId()
+        }
 
         val btnAddSet = findViewById<Button>(R.id.btnAddSet)
         val etSetTitle = findViewById<TextInputEditText>(R.id.etSetTitle)
@@ -49,22 +65,16 @@ class CreateSetActivity : AppCompatActivity() {
             if (setTitle.isNotEmpty() && cardAdapter.getCards().size > 0 && allCardsFull) {
                 var title = setTitle
                 var type = ""
-                for ((counter, card) in cardAdapter.getCards().withIndex()) {
-                    type += card
-                    if (counter != cardAdapter.getCards().size - 1) {
-                        type += ","
-                    }
-                }
-                val res : Response<CreateCardSetResponse> = runBlocking {
-                    Log.d("inhere", "here")
-                    return@runBlocking api.addSet(CreateCardSetEntity(0, title, type))
+
+                val response : Response<CreateCardSetResponse> = runBlocking {
+                    return@runBlocking api.addSet(CreateCardSetEntity(userid, title, type))
                 }
 
-                Log.d("res", res.toString())
-                // TODO: Compute a new set id, probably take largest current ID for user and increment
-                // val setId = 0
-                // val set = Set(setId, setTitle, cardAdapter.getCards().size, cardAdapter.getCards(), 0, SetCategory.CREATED_SET)
-                // TODO: Upload SET to Database
+                for (card in cardAdapter.getCards().withIndex()) {
+                    runBlocking {
+                        return@runBlocking api.createCard(CreateCardEntity(card.value.phrase, card.value.phrase, response.body()!!.set!!.set_id))
+                    }
+                }
                 finish()
             }
         }
