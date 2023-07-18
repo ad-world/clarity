936e5863faedc433c9feaee354f5137a89674244
Dhir Patel <dhirpatel133@gmail.com>
2023-07-18 17:11:28 -0400

    - implemented functionality of adding a new class.

--- android/app/src/main/java/com/example/clarity/classroompage/ClassroomFragment.kt
+++ android/app/src/main/java/com/example/clarity/classroompage/ClassroomFragment.kt
@@ -17,6 +17,8 @@ import com.example.clarity.IndexActivity
 import com.example.clarity.R
 import com.example.clarity.SessionManager
 import com.example.clarity.sdk.ClaritySDK
+import com.example.clarity.sdk.CreateClassroomEntity
+import com.example.clarity.sdk.CreateClassroomResponse
 import com.example.clarity.sdk.GetClassroomResponse
 import com.example.clarity.sdk.JoinClassroomEntity
 import com.example.clarity.sdk.JoinClassroomResponse
@@ -32,41 +34,70 @@ import java.util.concurrent.atomic.AtomicBoolean
  */
 class ClassroomFragment : Fragment() {
 
-        // placeholder listview data
-        private lateinit var recyclerView: RecyclerView
-        private lateinit var classAdapter: ClassAdapter
-        private var uid: Int = 0
-        private val api = ClaritySDK().apiService
-        private val sessionManager: SessionManager by lazy { SessionManager(requireContext()) }
+    // placeholder listview data
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var classAdapter: ClassAdapter
+    private var uid: Int = 0
+    private val api = ClaritySDK().apiService
+    private val sessionManager: SessionManager by lazy { SessionManager(requireContext()) }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_classroom, container, false)
+
+        // connect the classroom list with the frontend design and set its layout
+        recyclerView = view.findViewById(R.id.rvClasses)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //get the userID from the global session manager
+        lifecycleScope.launch {
+            uid = sessionManager.getUserId()
         }
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            // Inflate the layout for this fragment
-            val view = inflater.inflate(R.layout.fragment_classroom, container, false)
+        // declare a list to show the different classrooms user is currently enrolled in
+        val classes: MutableList<Pair<String, String>> = mutableListOf()
 
-            // connect the classroom list with the frontend design and set its layout
-            recyclerView = view.findViewById(R.id.rvClasses)
-            recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        // toggle switch between student and teacher classes
+        var materialSwitch = view.findViewById<MaterialSwitch>(R.id.toggleClass)
 
-            //get the userID from the global session manager
-            lifecycleScope.launch {
-                uid = sessionManager.getUserId()
+        // show default student list when classroom page loads (toggle button is checked
+        if (materialSwitch.isChecked) {
+            // make api call to get the list of student class names from the backend
+            var classesResponse : Response<GetClassroomResponse> = runBlocking {
+                return@runBlocking api.getClassesStudent(uid.toString())
             }
+            // confirm if response was received
+            if (classesResponse != null && classesResponse.body()?.response == StatusResponse.Success) {
+                val retrievedClassList = classesResponse.body()?.id // list of class names
+                if (retrievedClassList != null) {
+                    classes.clear()
+                    // loop over each class name item and add it to the classes list
+                    for (className in retrievedClassList) {
+                        classes.add(Pair(className.name, className.code))
+                    }
+                }
+            }
+            // adding the list classes to the recycler view (with recycler custom ClassAdapter)
+            classAdapter = ClassAdapter(classes) { className ->
+                val intent = Intent(requireContext(), Classroom::class.java)
+                intent.putExtra("className", className.first)
+                startActivity(intent)
+            }
+            recyclerView.adapter = classAdapter
+        }
 
-            // declare a list to show the different classrooms user is currently enrolled in
-            val classes: MutableList<Pair<String, String>> = mutableListOf()
-
-            // toggle switch between student and teacher classes
-            var materialSwitch = view.findViewById<MaterialSwitch>(R.id.toggleClass)
-
-            // show default student list when classroom page loads (toggle button is checked
-            if (materialSwitch.isChecked) {
+        // update clas list according to toggle switch
+        materialSwitch.setOnCheckedChangeListener { switch, isChecked ->
+            // if toggle is on, show student classes (i.e. classes user has joined)
+            if (isChecked) {
+                switch.text = "Show Your Classes"
                 // make api call to get the list of student class names from the backend
                 var classesResponse : Response<GetClassroomResponse> = runBlocking {
                     return@runBlocking api.getClassesStudent(uid.toString())
@@ -90,12 +121,43 @@ class ClassroomFragment : Fragment() {
                 }
                 recyclerView.adapter = classAdapter
             }
+            // otherwise show teacher's class (i.e. classes that the user has created)
+            else {
+                switch.text = "Show Joined Classed"
+                // make api call to get the list of teacher's class names from the backend
+                var classesResponse : Response<GetClassroomResponse> = runBlocking {
+                    return@runBlocking api.getClasses(uid.toString())
+                }
+                // confirm if response was received
+                if (classesResponse != null && classesResponse.body()?.response == StatusResponse.Success) {
+                    val retrievedClassList = classesResponse.body()?.id // list of class names
+                    if (retrievedClassList != null) {
+                        classes.clear()
+                        // loop over each class name item and add it to the classes list
+                        for (className in retrievedClassList) {
+                            classes.add(Pair(className.name, className.code))
+                        }
+                    }
+                }
+                // adding the list classes to the recycler view (with recycler custom ClassAdapter)
+                classAdapter = ClassAdapter(classes) { className ->
+                    val intent = Intent(requireContext(), Classroom::class.java)
+                    intent.putExtra("className", className.first)
+                    startActivity(intent)
+                }
+                recyclerView.adapter = classAdapter
+            }
+        }
 
-            // update clas list according to toggle switch
-            materialSwitch.setOnCheckedChangeListener { switch, isChecked ->
-                // if toggle is on, show student classes (i.e. classes user has joined)
-                if (isChecked) {
-                    switch.text = "Show Your Classes"
+        // creating a listener action for the join class button
+        val joinClassButton = view.findViewById<Button>(R.id.btnJoinClass)
+        joinClassButton.setOnClickListener {
+            // if joining class was a success, showJoinClassDialog should return true
+            showJoinClassDialog { successfullyJoined ->
+                if (successfullyJoined) {
+                    // set switch to true to show student classes
+                    materialSwitch.isChecked = true
+                    materialSwitch.text = "Show Your Classes"
                     // make api call to get the list of student class names from the backend
                     var classesResponse : Response<GetClassroomResponse> = runBlocking {
                         return@runBlocking api.getClassesStudent(uid.toString())
@@ -119,9 +181,29 @@ class ClassroomFragment : Fragment() {
                     }
                     recyclerView.adapter = classAdapter
                 }
-                // otherwise show teacher's class (i.e. classes that the user has created)
-                else {
-                    switch.text = "Show Joined Classed"
+            }
+        }
+        // creating a listener action for the create class button
+        val createClassButton = view.findViewById<Button>(R.id.btnCreateClass)
+        createClassButton.setOnClickListener {
+            var classCode = mutableListOf<String>()
+            joinClassDialog(classCode) {classCreated ->
+                if (classCreated && classCode.size > 0) {
+                    val code = classCode[0] // store class code result in a variable
+
+                    // create a popup to notify the user about the new class code created
+                    val dialogBuilder = AlertDialog.Builder(context)
+                    dialogBuilder.setTitle("Label Dialog")
+                    dialogBuilder.setMessage("Your classroom code is $code")
+                    dialogBuilder.setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    val dialog = dialogBuilder.create()
+                    dialog.show()
+
+                    // get list of classes that are made by current user (i.e. current user is the teacher)
+                    materialSwitch.isChecked = false
+                    materialSwitch.text = "Show Joined Classed"
                     // make api call to get the list of teacher's class names from the backend
                     var classesResponse : Response<GetClassroomResponse> = runBlocking {
                         return@runBlocking api.getClasses(uid.toString())
@@ -146,88 +228,83 @@ class ClassroomFragment : Fragment() {
                     recyclerView.adapter = classAdapter
                 }
             }
-
-            // creating a listener action for the join class button
-            val joinClassButton = view.findViewById<Button>(R.id.btnJoinClass)
-            joinClassButton.setOnClickListener {
-                // if joining class was a success, showJoinClassDialog should return true
-                showJoinClassDialog { successfullyJoined ->
-                    if (successfullyJoined) {
-                        // set switch to true to show student classes
-                        materialSwitch.isChecked = true
-                        materialSwitch.text = "Show Your Classes"
-                        // make api call to get the list of student class names from the backend
-                        var classesResponse : Response<GetClassroomResponse> = runBlocking {
-                            return@runBlocking api.getClassesStudent(uid.toString())
-                        }
-                        // confirm if response was received
-                        if (classesResponse != null && classesResponse.body()?.response == StatusResponse.Success) {
-                            val retrievedClassList = classesResponse.body()?.id // list of class names
-                            if (retrievedClassList != null) {
-                                classes.clear()
-                                // loop over each class name item and add it to the classes list
-                                for (className in retrievedClassList) {
-                                    classes.add(Pair(className.name, className.code))
-                                }
-                            }
-                        }
-                        // adding the list classes to the recycler view (with recycler custom ClassAdapter)
-                        classAdapter = ClassAdapter(classes) { className ->
-                            val intent = Intent(requireContext(), Classroom::class.java)
-                            intent.putExtra("className", className.first)
-                            startActivity(intent)
-                        }
-                        recyclerView.adapter = classAdapter
-                    }
-                }
-            }
-
-            // creating a listener action for the create class button
-            val createClassButton = view.findViewById<Button>(R.id.btnCreateClass)
-            createClassButton.setOnClickListener {
-                val intent = Intent(activity, Classroom::class.java)
-                startActivity(intent)
-            }
-            return view
         }
+        return view
+    }
 
-        // create a alert dialog when the join class button is pressed. Give the user the option to
-        // enter the class code of the class they wish to join.
-        private fun showJoinClassDialog(callback: (Boolean) -> Unit) {
-            val dialogBuilder = AlertDialog.Builder(requireContext())
-            dialogBuilder.setTitle("Join Class")
-            val input = EditText(requireContext())
-            input.hint = "Enter the classroom code" // add placeholder text to the textfield
-            dialogBuilder.setView(input) // add textfeild to the dialog box
-            // setting input type to text (allow the user to enter any textual data)
-            input.inputType = InputType.TYPE_CLASS_TEXT
+    // create a alert dialog when the join class button is pressed. Give the user the option to
+    // enter the class code of the class they wish to join.
+    private fun showJoinClassDialog(callback: (Boolean) -> Unit) {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setTitle("Join Class")
+        val input = EditText(requireContext())
+        input.hint = "Enter Classroom Code" // add placeholder text to the textfield
+        dialogBuilder.setView(input) // add textfeild to the dialog box
+        // setting input type to text (allow the user to enter any textual data)
+        input.inputType = InputType.TYPE_CLASS_TEXT
 
-            // take action if user joins a class
-            dialogBuilder.setPositiveButton("Join") { dialog, _ ->
-                val classCode = input.text.toString() // get user's class code input
-                // create request body for join classroom api
-                var req = JoinClassroomEntity(classCode, uid.toString())
-                // get response from join classroom api
-                var response : Response<JoinClassroomResponse> = runBlocking {
-                    return@runBlocking api.joinClass(req)
-                }
-                // check if the user entered the correct code (i.e. if backend returned the correct response)
-                if (response.body()?.response == StatusResponse.Success &&
-                    response.body()?.id == "Classroom Joined") {
-                    // add to list of joined classroom
-                    callback(true)
-                    // switch screen and switch to the class page
-//                    val intent = Intent(activity, Classroom::class.java)
-//                    startActivity(intent)
-                }
+        // take action if user joins a class
+        dialogBuilder.setPositiveButton("Join") { dialog, _ ->
+            val classCode = input.text.toString() // get user's class code input
+            // create request body for join classroom api
+            var req = JoinClassroomEntity(classCode, uid.toString())
+            // get response from join classroom api
+            var response : Response<JoinClassroomResponse> = runBlocking {
+                return@runBlocking api.joinClass(req)
             }
-            // do nothing if user cancels the dialog box
-            dialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
-                callback(false)
-                dialog.dismiss()
+            // check if the user entered the correct code (i.e. if backend returned the correct response)
+            if (response.body()?.response == StatusResponse.Success &&
+                response.body()?.id == "Classroom Joined") {
+                // add to list of joined classroom
+                callback(true)
             }
-            // create and sow the dialog
-            val dialog = dialogBuilder.create()
-            dialog.show()
         }
+        // do nothing if user cancels the dialog box
+        dialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
+            callback(false)
+            dialog.dismiss()
+        }
+        // create and sow the dialog
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
+
+    // create a alert dialog when the join class button is pressed. Give the user the option to
+    // enter the class code of the class they wish to join.
+    private fun joinClassDialog(codeList: MutableList<String>, callback: (Boolean) -> Unit) {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setTitle("Add Class")
+        val input = EditText(requireContext())
+        input.hint = "Enter Classroom Name" // add placeholder text to the textfield
+        dialogBuilder.setView(input) // add textfeild to the dialog box
+        // setting input type to text (allow the user to enter any textual data)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+
+        // take action if user joins a class
+        dialogBuilder.setPositiveButton("Add Class") { dialog, _ ->
+            val className = input.text.toString() // get user's class code input
+            // create request body for join classroom api
+            var req = CreateClassroomEntity(className, uid)
+            // get response from join classroom api
+            var response : Response<CreateClassroomResponse> = runBlocking {
+                return@runBlocking api.createClass(req)
+            }
+            // check if the user entered the correct code (i.e. if backend returned the correct response)
+            if (response.body()?.response == StatusResponse.Success) {
+                val classCode = response.body()?.id
+                if (classCode != null) {
+                    codeList.add(classCode)
+                }
+                callback(true)
+            }
+        }
+        // do nothing if user cancels the dialog box
+        dialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
+            callback(false)
+            dialog.dismiss()
+        }
+        // create and sow the dialog
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
 }
