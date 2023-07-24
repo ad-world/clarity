ebcc4bcb7608376d9dae1078c5e935199b6383d0
Saumya Khati <65821287+SaumyaKhati@users.noreply.github.com>
2023-07-23 22:40:53 -0400

    Added new set functionality (#61)
    - Get public card sets from CardSet table.
    - Clone public sets.
    - Get set data (full version: with metadata and card objects).
    - Changed /getSets endpoint to /getSetIDs to reflect true data being
      returned.
    - Added/Modified Postman Collection as needed.
    - Removed AUTOINCREMENT from CardSet since it's not needed and just
      introduces additional overhead. See
      https://www.sqlite.org/autoinc.html for more details.

--- backend/src/main/sql/ddl/CREATE_CARDSET.sql
+++ backend/src/main/sql/ddl/CREATE_CARDSET.sql
@@ -2,13 +2,15 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS CardSet (
     -- set_id is a reserved keyword, so using [] so SQL interprets it as a column name.
-    [set_id] INTEGER PRIMARY KEY AUTOINCREMENT,
+    [set_id] INTEGER PRIMARY KEY,
     creator_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     type VARCHAR(10) NOT NULL,
     is_public_ind INTEGER NOT NULL,
     likes INTEGER NOT NULL,
+    cloned_from_set INTEGER NULL, -- Card Sets can be newly created, or cloned from a public set.
     CHECK ( is_public_ind = 0 OR is_public_ind = 1 ),
     CHECK ( likes >= 0),
-    FOREIGN KEY (creator_id) REFERENCES User(user_id)
+    FOREIGN KEY (creator_id) REFERENCES User(user_id),
+    FOREIGN KEY (cloned_from_set) REFERENCES CardSet([set_id])
 )
